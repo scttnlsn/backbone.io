fccde85fdb4c95d4352022e30080935013f73f59
Scott Nelson <scottbnel@gmail.com>
2011-11-16 12:37:29 -0500

    Updated example app

--- example/app.js
+++ example/app.js
@@ -6,5 +6,5 @@ app.use(express.static(__dirname));
 app.listen(3000);
 console.log('http://localhost:3000/');
 
-var Backend = require('../lib/backends/memory');
+var Backend = require('../backends/memory');
 backboneio.listen(app, { messages: new Backend() });
