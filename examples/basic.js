cb4d2ba4ef278f0896392ea2cf92b254f04da617
Richard Assar <richard@musemantik.com>
2013-03-09 23:38:14 +0000

    Rewrite example to test mongooseStore

--- examples/basic.js
+++ examples/basic.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var backboneio = require('../lib/index');
+var mongoose = require('mongoose');
 
 var app = express.createServer();
 app.use(express.static(__dirname));
@@ -8,6 +9,18 @@ app.listen(3000);
 console.log('http://localhost:3000/');
 
 var messages = backboneio.createBackend();
-messages.use(backboneio.middleware.memoryStore());
 
-backboneio.listen(app, { messages: messages });
+mongoose.connect('localhost', 'backboneio-test');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var messageSchama = mongoose.Schema({text: 'string'});
+var MessageModel = mongoose.model('Message', messageSchama);
+
+db.once('open', function callback () {
+	messages.use(backboneio.middleware.mongooseStore(MessageModel));
+
+	backboneio.listen(app, { messages: messages });
+})
