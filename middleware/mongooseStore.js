9f3cd344a8425e5b107d7720f5ba3ac3fd3f519a
Richard Assar <richard@musemantik.com>
2013-03-09 23:36:52 +0000

    mongooseStore _id to id bugfix

--- middleware/mongooseStore.js
+++ middleware/mongooseStore.js
@@ -1,44 +1,55 @@
 module.exports = function(Model) {
     return function(req, res, next) {
+        var convertItem = function(item) {
+            item = item.toObject();
+
+            item.id = item._id;
+
+            delete item._id;
+            delete item.__v;
+
+            return item;
+        };
+
         var callback = function(err, result) {
             if (err) return next(err);
-            res.end(result);
+
+            if(result instanceof Array)
+                res.end(result.map(convertItem));
+            else
+                res.end(convertItem(result))
         };
-        
+
         var crud = {
             create: function() {
                 Model.create(req.model, callback);
             },
-            
+
             read: function() {
-                if (req.model._id) {
-                    Model.findById(req.model._id, callback);
+                if (req.model.id) {
+                    Model.findById(req.model.id, callback);
                 } else {
                     Model.find(callback);
                 }
             },
-            
+
             update: function() {
-                var model = {};
-                for (var key in req.model) {
-                    model[key] = req.model[key];
-                }
-                delete model._id;
-                
-                Model.update({ _id: req.model._id }, { '$set': model }, function(err) {
+                var model = convertItem(req.model);
+
+                Model.update({ _id: req.model.id }, { '$set': model }, function(err) {
                     if (err) return next(err);
                     res.end(req.model);
                 });
             },
-            
+
             delete: function() {
-                Model.remove({ _id: req.model._id }, function(err) {
+                Model.remove({ _id: req.model.id }, function(err) {
                     if (err) return next(err);
                     res.end(req.model);
                 });
             }
         };
-        
+
         if (!crud[req.method]) return next(new Error('Unsuppored method ' + req.method));
         crud[req.method]();
     }
