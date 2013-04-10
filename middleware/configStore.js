b63e9af0dd00a2e8225592ba65920a8609574734
José Luis Di Biase <josx@interorganic.com.ar>
2013-04-10 13:05:26 -0300

    Bind the whole model to node-config
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- middleware/configStore.js
+++ middleware/configStore.js
@@ -5,30 +5,25 @@ module.exports = function() {
     return function(req, res, next) {
         var crud = {
             create: function() {
-                //Assing configuration from models config property to config module
-                conf._extendDeep(conf, req.model.config);
+                //Assing configuration from models to node-config module
+                conf._extendDeep(conf, req.model);
                 res.end(req.model);
             },
             
             read: function() {
-                // Assing configuration from config module to models config property
-                req.model.config = conf;
+                // Assing configuration from node-config module to models
+                req.model = conf;
                 res.end(req.model);
             },
             
             update: function() {
-                console.log("---------------------");
-                console.log(JSON.stringify(req.model.config));
-                console.log("---------------------");
-                
-                // Assign req.model.config to conf 
-                conf._extendDeep(conf, req.model.config);
+                // Assign req.model to node-config
+                conf._extendDeep(conf, req.model);
                 res.end(req.model)
             },
             
             delete: function() {
-                req.model.config = {};
-                conf = {};
+                req.model = conf = {};
                 res.end(req.model);
             }
         };
