3a8dd16e359e498981f89761e54a0119e7274f6a
José Luis Di Biase <josx@interorganic.com.ar>
2013-04-30 23:23:26 -0300

    Adding node-config original values on models
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- middleware/configStore.js
+++ middleware/configStore.js
@@ -1,4 +1,5 @@
 module.exports = function() {
+    var _ = require('underscore');
     var conf = require('mbc-common').config;
     var self = this;
 
@@ -11,14 +12,15 @@ module.exports = function() {
             },
 
             read: function() {
-                // Assing configuration from node-config module to models
-                req.model = conf;
+                // Assing configuration and defaults from node-config module to models
+                req.model[0] = _.extend(conf, {type: 'config'} );
+                req.model[1] = _.extend(conf.getOriginalConfig(), { type: 'defaults'} );
                 res.end(req.model);
             },
 
             update: function() {
                 // Assign req.model to node-config
-                conf._extendDeep(conf, req.model);
+                conf._extendDeep(conf, req.model[0]);
                 res.end(req.model)
             },
 
@@ -27,7 +29,7 @@ module.exports = function() {
                     if(err) {
                         return res.end({'error':'An error has occurred on delete' + err});
                     }
-                    req.model = conf;
+                    req.model[0] = conf;
                     res.end(req.model);
                 });
             }
